921850a0bf2e8b4a2ebb7499ce6a65d65459683b
nhowlandatl <nhowland@outlook.com>
2020-12-17 01:56:47 -0500

    testing fb login

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,86 +1,205 @@
-import React, { useState, useEffect } from "react";
-import { Form, Container, Row, Col } from "react-bootstrap";
-import { Link, useHistory, withRouter } from "react-router-dom";
+// import React from "react";
+// import Amplify from "aws-amplify";
+// import { AmplifyAuthenticator } from "@aws-amplify/ui-react";
+// import awsexports from "../aws-exports";
 
-// Components
-import FacebookLogin from 'react-facebook-login';
-import Button from "react-bootstrap/Button";
-import "./Login.css";
+// Amplify.configure(awsexports);
 
-// AWS
-import Auth from 'aws-amplify';
+// const federated = {
+//   facebookAppId: "197530488546623", // app id goes here
+//   oauthConfig: {
+//     redirectSignIn: "https://localhost:3000/",
+//     redirectSignOut: "https://localhost:3000/",
+//   },
+// };
+
+// function Login() {
+//   return (
+//     <div className="Login">
+//       <AmplifyAuthenticator federated={federated}>
+//         <div>Hello world !!!! </div>
+//       </AmplifyAuthenticator>
+//     </div>
+//   );
+// }
+
+// export default Login;
+
+import React, { useEffect } from 'react';
+import { Auth } from 'aws-amplify';
+// To federated sign in from Facebook
+const Login = () => {
+
+    useEffect(() => {
+        if (!window.FB) createScript();
+    }, [])
+
+    const signIn = () => {
+        const fb = window.FB;
+        fb.getLoginStatus(response => {
+            if (response.status === 'connected') {
+                getAWSCredentials(response.authResponse);
+            } else {
+                fb.login(
+                    response => {
+                        if (!response || !response.authResponse) {
+                            return;
+                        }
+                        getAWSCredentials(response.authResponse);
+                    },
+                    {
+                        // the authorized scopes
+                        scope: 'public_profile,email'
+                    }
+                );
+            }
+        });
+    }
+
+    const getAWSCredentials = (response) => {
+            const { accessToken, expiresIn } = response;
+            const date = new Date();
+            const expires_at = expiresIn * 1000 + date.getTime();
+            if (!accessToken) {
+                return;
+            }
+
+            const fb = window.FB;
+            fb.api('/me', { fields: 'name,email' }, response => {
+                const user = {
+                    name: response.name,
+                    email: response.email
+                };
+
+                Auth.federatedSignIn('facebook', { token: accessToken, expires_at }, user)
+                .then(credentials => {
+                    console.log(credentials);
+                });
+            });
+        }
+
+    const initFB = () => {
+        const fb = window.FB;
+        console.log('FB SDK initialized');
+    }
+
+    const createScript = () => {
+      // load the sdk
+      window.fbAsyncInit = fbAsyncInit;
+      const script = document.createElement('script');
+      script.src = 'https://connect.facebook.net/en_US/sdk.js';
+      script.async = true;
+      script.onload = initFB;
+      document.body.appendChild(script);
+  }
+
+    const fbAsyncInit = () => {
+        // init the fb sdk client
+        const fb = window.FB;
+        fb.init({
+            appId   : '197530488546623',
+            cookie  : true,
+            xfbml   : true,
+            version : 'v2.11'
+        });
+    }
+
+    return (
+        <div>
+            <button onClick={signIn}>Sign in with Facebook</button>
+        </div>
+    );
+}
+
+export default Login;
+
+
+
+
+
+// import React, { useState, useEffect } from "react";
+// import { Form, Container, Row, Col } from "react-bootstrap";
+// import { Link, useHistory, withRouter } from "react-router-dom";
+
+// // Components
+// import FacebookLogin from 'react-facebook-login';
+// import Button from "react-bootstrap/Button";
+// import "./Login.css";
+
+// // AWS
+// import Amplify, { Auth } from 'aws-amplify';
 // import awsconfig from '../aws-exports';
 // Amplify.configure(awsconfig);
 
-function Login(props) {
-  const history = useHistory();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+// function Login(props) {
+//   const history = useHistory();
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
 
-  function validateForm() {
-    return email.length > 0 && password.length > 0;
-  }
+//   function validateForm() {
+//     return email.length > 0 && password.length > 0;
+//   }
 
-  async function handleSubmit(event) {
-    event.preventDefault();
+//   async function handleSubmit(event) {
+//     event.preventDefault();
   
-    try {
-      await Auth.signIn(email, password);
-      window.location.href="/"; 
-    } catch (e) {
-      alert(e.message);
-    }
-  }
+//     try {
+//       await Auth.signIn(email, password);
+//       window.location.href="/"; 
+//     } catch (e) {
+//       alert(e.message);
+//     }
+//   }
   
-  return (
-    <div className="Login">
-        {props.loggedIn === true &&
-        <div>
-            You are already logged in
-        </div>
-        }
-        {props.loggedIn === false &&
-        <Container> 
-            <Row className="justify-content-md-center">
-                <Col xs={5.5}>
-                    <div>Sign in with your social media account</div>
-                    <FacebookLogin onClick={() => Auth.federatedSignIn({provider: 'Facebook'})}></FacebookLogin>
-                    <div>We won't post to any of your accounts without asking first</div>
-                </Col>
-                <Col xs={1} className="my-auto">
-                    Or
-                </Col>
-                <Col xs={5.5}>
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group size="lg" controlId="email">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control
-                                autoFocus
-                                type="email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                        </Form.Group>
-                        <Form.Group size="lg" controlId="password">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control
-                                type="password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                            Forgot your password? Click here to reset
-                        </Form.Group>
-                        <Button block size="lg" type="submit" disabled={!validateForm()}>
-                        Login
-                        </Button>
-                        <Link to="/signup">Need an account? Sign up</Link>
-                    </Form>
-                </Col>
-            </Row>
-        </Container>
-        }
-    </div>
-  );
-}
+//   return (
+//     <div className="Login">
+//         {props.loggedIn === true &&
+//         <div>
+//             You are already logged in
+//         </div>
+//         }
+//         {props.loggedIn === false &&
+//         <Container> 
+//             <Row className="justify-content-md-center">
+//                 <Col xs={5.5}>
+//                     <div>Sign in with your social media account</div>
+//                     <FacebookLogin onClick={() => Auth.federatedSignIn({provider: 'Facebook'})}></FacebookLogin>
+//                     <div>We won't post to any of your accounts without asking first</div>
+//                 </Col>
+//                 <Col xs={1} className="my-auto">
+//                     Or
+//                 </Col>
+//                 <Col xs={5.5}>
+//                     <Form onSubmit={handleSubmit}>
+//                         <Form.Group size="lg" controlId="email">
+//                             <Form.Label>Email</Form.Label>
+//                             <Form.Control
+//                                 autoFocus
+//                                 type="email"
+//                                 value={email}
+//                                 onChange={(e) => setEmail(e.target.value)}
+//                             />
+//                         </Form.Group>
+//                         <Form.Group size="lg" controlId="password">
+//                             <Form.Label>Password</Form.Label>
+//                             <Form.Control
+//                                 type="password"
+//                                 value={password}
+//                                 onChange={(e) => setPassword(e.target.value)}
+//                             />
+//                             Forgot your password? Click here to reset
+//                         </Form.Group>
+//                         <Button block size="lg" type="submit" disabled={!validateForm()}>
+//                         Login
+//                         </Button>
+//                         <Link to="/signup">Need an account? Sign up</Link>
+//                     </Form>
+//                 </Col>
+//             </Row>
+//         </Container>
+//         }
+//     </div>
+//   );
+// }
 
-export default withRouter(Login)
+// export default withRouter(Login)
