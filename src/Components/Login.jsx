f75fafd42f5745972416228d03a976c05a96163b
nhowlandatl <nhowland@outlook.com>
2020-12-16 22:04:34 -0500

    modifying facebook page

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -8,9 +8,9 @@ import Button from "react-bootstrap/Button";
 import "./Login.css";
 
 // AWS
-import Amplify, { Auth } from 'aws-amplify';
-import awsconfig from '../aws-exports';
-Amplify.configure(awsconfig);
+import Auth from 'aws-amplify';
+// import awsconfig from '../aws-exports';
+// Amplify.configure(awsconfig);
 
 function Login(props) {
   const history = useHistory();
